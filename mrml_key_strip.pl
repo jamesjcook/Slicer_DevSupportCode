6c1ebfe8cc06da3333ce558938e188689b01113d
James Cook <jamesjcook@users.noreply.github.com>
2018-10-04 17:19:13 -0400

    Clenaed up input/ouput settings and which nodes are kept to support new vesions of slicer which include subject hierarchy.

--- mrml_key_strip.pl
+++ mrml_key_strip.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 # mrml_key_strip.pl
 # eliminate keys we dont want in our mrml file.
-# takes incolor table, inmrml could specifiy outmrml as well
+# takes inmrml could specifiy outmrml as well
 
 use strict;
 use warnings;
@@ -54,11 +54,11 @@ if ( ! defined $rename_type ) {
 {
     my ($p,$n,$e)=fileparts($inmrml,2);
     if ($rename_type eq 'clean' ) { 
-	$outmrml=$p.$n."_template".$e if ( ! defined $outmrml ) ;
-	$outmrml_n=$p.$n."_mhn".$e;
+	$outmrml_n=$p.$n."_template".$e;
+	$outmrml=$p.$n."_mhn".$e  if ( ! defined $outmrml ) ;
     } else {
-	$outmrml=$p.$n."_template".$e if ( ! defined $outmrml ) ;
-	$outmrml_n=$p.$n."_$rename_type".$e;
+	$outmrml_n=$p.$n."_template".$e ;
+	$outmrml=$p.$n."_$rename_type".$e if ( ! defined $outmrml ) ;
     }
     
     print("Auto mrml out will be \"$outmrml\".\n") if ( ! defined $outmrml ) ;
@@ -70,14 +70,31 @@ my ($xml_data,$xml_parser)=xml_read($inmrml,'giveparser');
 #    my ($n,$p,$e)=fileparts($inpath);
 #     $color_table_out=$p.$n."_$rename_type".$e;
 #}
+# these are our fibebundle nodes as noted in slicer
+#vtkMRMLFiberBundleNode
+#vtkMRMLFiberBundleTubeDisplayNode
+#vtkMRMLFiberBundleLineDisplayNode
+#vtkMRMLFiberBundleGlyphDisplayNode
+#vtkMRMLLinearTransformNode
 
+
+# Fiber bundle expansion didnt quite work.
+# Defacto setup with a clean settings file, no tracts show up. There is a loading glitch in that instance(maybe missing extensions?)
+# After manually using correct settings, tube display is on by default.
+# Maybe deleting those nodes will address that, howver its not clear that is the right answer.
+# 
+
+my @node_types_preserved=qw(MRML LinearTransform TransformStorage Version UserTags SubjectHierarchy SubjectHierarchyItem);
+my @model_node_types=qw(ModelHierarchy ModelDisplay Model ModelStorage );
+my @fiber_node_types=qw(FiberBundle DiffusionTensorDisplayProperties FiberBundleTubeDisplayNode FiberBundleLineDisplayNode FiberBundleGlyphDisplayNode);
+push(@node_types_preserved,@model_node_types);
+push(@node_types_preserved,@fiber_node_types);
 #print("colortable ".($#color_table+1)." lines loaded\n");
-mrml_clear_nodes($xml_data,("ModelHierarchy","ModelDisplay","Model","ModelStorage","Version", "UserTags"));
-#, "TransformStorage", "LinearTransform"));
-#mrml_clear_nodes($xml_data,("ModelHierarchy","ModelDisplay","Model","ModelStorage","Version", "UserTags", "TransformStorage", "LinearTransform"));
-mrml_to_file($xml_data,'  ',0,'pretty','',$outmrml_n);
+mrml_clear_nodes($xml_data,@node_types_preserved);
+mrml_to_file($xml_data,'  ',0,'pretty','',$outmrml);
+
+# not sure the purpo
 if( $rename_type eq 'modelfile' || $rename_type eq 'ontology' || $rename_type eq 'abrev') {
-    mrml_clear_nodes($xml_data,("ModelHierarchy","ModelDisplay","Version", "UserTags"));
-    mrml_to_file($xml_data,'  ',0,'pretty','',$outmrml);
-#    write_array_to_file($color_table_out,\@color_table);
+#    mrml_clear_nodes($xml_data,("ModelHierarchy","ModelDisplay","Version", "UserTags"));
+#    mrml_to_file($xml_data,'  ',0,'pretty','',$outmrm_n);
 }
